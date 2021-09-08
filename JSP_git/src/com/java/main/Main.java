76fb3a6dbc2c7a4dd099e0c2064de6c4014b69ff
rabo2 <raboy500@gmail.com>
2021-09-08 13:37:08 +0900

    hotfix_1

--- JSP_git/src/com/java/main/Main.java
+++ JSP_git/src/com/java/main/Main.java
@@ -3,5 +3,7 @@ package com.java.main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("main");
+		System.out.println("hellooooooooo");
+		System.out.println("hotfix_1");
 	}
 }
